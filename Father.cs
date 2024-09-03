d5bc6398e4c6eae65243fa65eb3a76c19f542b24
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2024-09-03 08:19:08 +0530

    Father and Son assignment

---
+++ Father.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSDFundamentals
+{
+    public class Father
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string EarnMoney()
+        {
+            return "Handful Money";
+        }
+    }
+
+    public class Son :Father
+    {
+        public string GoingSchool()
+        {
+            return "Govt School";
+        }
+    }
+
+    public class Dauhter : Father
+    {
+        public string GoingTOCollege()
+        {
+            return "IT Enginnering College";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,38 @@
     {
         static void Main(string[] args)
         {
-           
-            Book book = new Book(10);
+            #region COnstructor Chain  
+            //Book book = new Book(10);
 
-            book.DisplayProgramFlow();
+            //book.DisplayProgramFlow();
 
-            Console.ReadLine();
+            //Console.ReadLine();
+
+            #endregion
+
+            #region Inhertiance Examples
+
+            Father father = new Father() { Id=100, Name="Father " };
+
+
+            Son son = new Son() { Id =1000, Name ="Son" };
+            
+
+            //Dauhter dauhter = new Dauhter();
+
+            father = son;
+
+            Console.WriteLine(father.Id); // Since son is assigned in father . you will get only son's properties
+            Console.WriteLine(father.Name);
+
+            //To To get son properties stored in father , you have convert or cast to original or Child type to GET THE SON PROPERTIES
+
+            Son s1=(Son)father;
+
+            s1.GoingSchool(); // the child or son property is called from father or parent object.
+
+
+            #endregion
         }
     }
 }
